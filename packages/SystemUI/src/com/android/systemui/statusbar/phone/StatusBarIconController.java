c82f4f2ad40ebaf9b0a06a2c5deb7c7517121400
Jake Weinstein <xboxlover360@gmail.com>
2017-04-24 19:42:08 -0400

    Revert "SystemUI:Unable to see Power Percentage icon"
    * Revert more CAF battery styles
    
    This reverts commit a896165ef6778ebb5ab73c2ab5cb73e0d0bab373.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -537,7 +537,6 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         mBatteryMeterView.setDarkIntensity(
                 isInArea(mTintArea, mBatteryMeterView) ? mDarkIntensity : 0);
         mClock.setTextColor(getTint(mTintArea, mClock, mIconTint));
-        mBatteryLevelView.setTextColor(getTint(mTintArea, mBatteryLevelView, mIconTint));
     }
 
     public void appTransitionPending() {
